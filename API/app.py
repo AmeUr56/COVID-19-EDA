a63f8fa829be698829a7e719f4d5beb34136b485
Ameur56 <boughershaouari@gmail.com>
2024-12-10 12:34:29 +0100

    Fixed API Bugs

--- API/app.py
+++ API/app.py
@@ -3,29 +3,32 @@ from flask import Flask,request,jsonify,make_response
 app = Flask(__name__)
 app.secret_key = "0123456789"
 
+raw_data = None
+processed_data = None
+
 @app.route("/raw_data",methods=["GET","POST"])
 def raw_data():
-    global raw_data
-    
     if request.method == "POST":
-        raw_data = request.form.get("raw_data")
+        global raw_data
+        raw_data = request.get_json()
+                    
         if not raw_data:
             response = make_response("Raw Data not provided.",400)
 
-        response = make_response("Raw Data Uploded Succefully.",201)
+        response = make_response("Raw Data Uploaded Succefully.",201)
         return response
     
-    if processed_data is None:
+    if raw_data is None:
         return jsonify({"error": "No raw data available."}), 404
     
-    return jsonify({"raw_data": raw_data})
+    return raw_data
 
 @app.route("/processed_data",methods=["GET","POST"])
 def processed_data():
-    global processed_data
-    
     if request.method == "POST":
-        processed_data = request.form.args.get("processed_data")
+        global processed_data
+        processed_data = request.get_json()
+        
         if not processed_data:
             response = make_response("Processed Data not provided.",400)
             return response
@@ -36,4 +39,4 @@ def processed_data():
     if processed_data is None:
         return jsonify({"error": "No processed data available."}), 404
     
-    return jsonify({"processed_data":processed_data})
+    return processed_data
